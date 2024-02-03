fe76092e222c0808924aec3efb5524129cd65476
vinoth <vinoth@gmail.com>
2024-02-03 18:44:49 +0530

    Version 1 initialized and updated responsive

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import FinalCards from "./Components/FinalCards";
+import ImageContent from "./Components/ImageContent";
+import ImageComponentCard from "./Components/ImageContentCard";
+
+function App() {
+  return (
+    <>
+      <ImageComponentCard />
+    </>
+  );
+}
+
+export default App;
